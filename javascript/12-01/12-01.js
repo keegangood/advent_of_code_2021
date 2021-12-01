c736d98312a5f69c155f63720ad00cd93249e4ed
keegangood <keegood8@gmail.com>
2021-12-01 11:55:12 -0800

    day 1, part 2

--- javascript/12-01/12-01.js
+++ javascript/12-01/12-01.js
@@ -1,5 +1,11 @@
-let data = require('./data.json')
+let data = require("./data.json");
 
-part1 = data.filter((datum, i, arr) => arr[i] > arr[i-1]).length
+const part1 = data.filter((datum, i, arr) => arr[i] > arr[i - 1]).length;
+// console.log(part1) // 1676
 
-console.log(part1)
+const part2 = data.filter(
+  (_, i, arr) =>
+    arr.slice(i + 1, i + 4).reduce((x, y) => x + y, 0) >
+    arr.slice(i, i + 3).reduce((x, y) => x + y, 0)
+).length;
+// console.log(part2) // 1706
