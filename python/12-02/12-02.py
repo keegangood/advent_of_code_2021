5f71ea7f19249d2839e4eb1fcf18da1310915cf9
Keegan Good <keegood8@gmail.com>
2021-12-02 10:02:11 -0800

    remove json generation and added part2 output in comment

--- python/12-02/12-02.py
+++ python/12-02/12-02.py
@@ -3,10 +3,6 @@ import json
 with open('data.txt', 'r') as data_file:
     data = [d for d in data_file.read().split('\n')]
 
-with open('data.json', 'w') as file:
-    file.write(json.dumps(data, indent=2))
-
-
 def part1(data):
     position = {
         'forward': 0,
@@ -49,4 +45,4 @@ def part2(data):
 
     return position['forward'] * position['depth']
 
-# print(part2(data))
+# print(part2(data)) # 1963088820
