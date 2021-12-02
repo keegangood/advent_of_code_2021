94e347f9b1b8c31632de6467bfecc3b1dc5ac289
Keegan Good <keegood8@gmail.com>
2021-12-02 10:12:25 -0800

    day 2, part 2

--- javascript/12-02/12-02.js
+++ javascript/12-02/12-02.js
@@ -26,4 +26,36 @@ const part1 = data => {
   return position.depth * position.forward
 }
 
-console.log(part1(data))
+// console.log(part1(data)) // 1714680
+
+const part2 = data => {
+  let position = {
+    aim: 0,
+    forward: 0,
+    depth: 0
+  }
+
+  data.forEach(move => {
+    let [direction, value] = move.split(' ')
+    value = parseInt(value)
+
+    switch (direction) {
+      case 'forward':
+        position.forward += value
+        position.depth += position.aim * value
+        break
+      case 'up':
+        position.aim -= value
+        break
+      case 'down':
+        position.aim += value
+        break
+      default:
+        break
+    }
+  })
+
+  return position.depth * position.forward
+}
+
+// console.log(part2(data)) // 1963088820
