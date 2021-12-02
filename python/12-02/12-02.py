47a1f3b2424a4ba75ef593af54ab6579b658d808
Keegan Good <keegood8@gmail.com>
2021-12-02 09:38:58 -0800

    day 2, part 2

--- python/12-02/12-02.py
+++ python/12-02/12-02.py
@@ -27,3 +27,26 @@ def part1(data):
 
 # print(part1(data)) # 1714680
 
+
+def part2(data):
+    position = {
+        'aim': 0,
+        'forward': 0,
+        'depth': 0
+    }
+
+    for move in data:
+        direction, value = move.split(' ')
+        value = int(value)
+        match direction:
+            case 'forward':
+                position['forward'] += value
+                position['depth'] += position['aim'] * value
+            case 'up':
+                position['aim'] -= value
+            case 'down':
+                position['aim'] += value
+
+    return position['forward'] * position['depth']
+
+# print(part2(data))
