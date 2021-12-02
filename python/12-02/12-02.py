2f6f0fd0038f3e92a808ae681e1f3a05c728dfdd
Keegan Good <keegood8@gmail.com>
2021-12-02 09:38:44 -0800

    day 2, part 1

---
+++ python/12-02/12-02.py
@@ -0,0 +1,29 @@
+import json
+
+with open('data.txt', 'r') as data_file:
+    data = [d for d in data_file.read().split('\n')]
+
+with open('data.json', 'w') as file:
+    file.write(json.dumps(data, indent=2))
+
+
+def part1(data):
+    position = {
+        'forward': 0,
+        'depth': 0
+    }
+    for move in data:
+        direction, distance = move.split(' ')
+        distance = int(distance)
+        match direction:
+            case 'forward':
+                position['forward'] += distance
+            case 'up':
+                position['depth'] -= distance
+            case 'down':
+                position['depth'] += distance
+    
+    return position['forward'] * position['depth']
+
+# print(part1(data)) # 1714680
+
