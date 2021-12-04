c1d73d22ce88ecb2196f2d094b410e86c4821a69
Keegan Good <keegood8@gmail.com>
2021-12-04 11:51:04 -0800

    renamed

--- python/12-01/part2.py
+++ python/12-01/part2.py
@@ -1,10 +1,6 @@
-
 with open('data.txt', 'r') as data_file:
     data = [int(d) for d in data_file.read().split('\n')]
 
-# PART 1
-find_increases = lambda data: len([data[i] for i in range(len(data)-1) if data[i] < data[i + 1]])
-# print(find_increases(data)) # 1676
 
 # PART 2
 def sonar_sweep(data):
